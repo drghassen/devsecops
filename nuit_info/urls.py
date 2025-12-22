3dbdb88c215ec82ffb119844e9dad285ac4c275c
drghassen <dridighassenbac2021@gmail.com>
2025-12-22 22:57:35 +0100

    fix css

--- nuit_info/urls.py
+++ nuit_info/urls.py
@@ -18,9 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", lambda request: redirect("/api/login/")),
     path("admin/", admin.site.urls),
     path("api/", include("iot.urls")),
 ]
+
+# Serve static files in development mode (even when using Daphne)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
